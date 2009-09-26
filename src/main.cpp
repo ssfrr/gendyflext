94935feb8d835080c97baadb568eb6e05ee1f1bb
Spencer Russell <spencer.f.russell@gmail.com>
2009-09-26 11:46:23 -0400

    swapped spaces for tabs

--- src/main.cpp
+++ src/main.cpp
@@ -20,11 +20,11 @@ using namespace std;
 
 // A flext dsp external ("tilde object") inherits from the class flext_dsp 
 class gendy:  public flext_dsp {
-    // flext macro magic
+	// flext macro magic
 	FLEXT_HEADER_S(gendy, flext_dsp, class_setup)
 
 	public:
-        gendy();	
+		gendy();	
 		~gendy();
 	
 	protected:
@@ -37,35 +37,35 @@ class gendy:  public flext_dsp {
 		static unsigned int gendy_count;
 		// instance ID
 		unsigned int id;
-        
-        // register the callbacks, and tell flext their calling format
+		
+		// register the callbacks, and tell flext their calling format
 		FLEXT_CALLBACK_F(set_frequency)
-        FLEXT_CALLBACK_I(set_num_breakpoints)
-        FLEXT_CALLBACK_F(set_h_step)
-        FLEXT_CALLBACK_F(set_v_step)
-        FLEXT_CALLBACK_F(set_h_pull)
-        FLEXT_CALLBACK_F(set_v_pull)
-        FLEXT_CALLBACK(set_interpolation_lin)
-        FLEXT_CALLBACK(set_interpolation_cubic)
-        FLEXT_CALLBACK(set_interpolation_spline)
-        FLEXT_CALLBACK(set_interpolation_sinc)
-        FLEXT_CALLBACK_I(set_debug)
+		FLEXT_CALLBACK_I(set_num_breakpoints)
+		FLEXT_CALLBACK_F(set_h_step)
+		FLEXT_CALLBACK_F(set_v_step)
+		FLEXT_CALLBACK_F(set_h_pull)
+		FLEXT_CALLBACK_F(set_v_pull)
+		FLEXT_CALLBACK(set_interpolation_lin)
+		FLEXT_CALLBACK(set_interpolation_cubic)
+		FLEXT_CALLBACK(set_interpolation_spline)
+		FLEXT_CALLBACK(set_interpolation_sinc)
+		FLEXT_CALLBACK_I(set_debug)
 
 		// Message handling functions
-        void set_frequency(float new_freq);
-        void set_num_breakpoints(float num_breakpoints);
-        void set_h_step(float new_stepsize);
-        void set_v_step(float new_stepsize);
-        void set_h_pull(float new_pull);
-        void set_v_pull(float new_pull);
-        void set_interpolation_lin();
-        void set_interpolation_cubic();
-        void set_interpolation_spline();
-        void set_interpolation_sinc();
+		void set_frequency(float new_freq);
+		void set_num_breakpoints(float num_breakpoints);
+		void set_h_step(float new_stepsize);
+		void set_v_step(float new_stepsize);
+		void set_h_pull(float new_pull);
+		void set_v_pull(float new_pull);
+		void set_interpolation_lin();
+		void set_interpolation_cubic();
+		void set_interpolation_spline();
+		void set_interpolation_sinc();
 		void set_debug(int new_debug);
 
 		// Internal class methods
-        static void class_setup(t_classid thisclass);
+		static void class_setup(t_classid thisclass);
 		void set_interpolation(int interpolation);
 };
 unsigned int gendy::gendy_count = 0;
@@ -76,8 +76,8 @@ gendy::gendy() {
 	id = gendy_count;
 	if(debug)
 		post("gendy~ #%d: Constructor initiated", id);
-    AddInAnything("control input");    // control input
-    AddOutSignal("audio out");          // audio output
+	AddInAnything("control input");	// control input
+	AddOutSignal("audio out");		  // audio output
 	// TODO: error checking for memory allocation
 	debug = false;
 	if(debug)
@@ -95,18 +95,18 @@ gendy::~gendy() {
 void gendy::class_setup(t_classid thisclass) {
 	// associate methods with incoming messages on inlet 0
 	// numerical input is taken as frequency
-    FLEXT_CADDMETHOD(thisclass, 0, set_frequency);
-    FLEXT_CADDMETHOD_(thisclass, 0, "breakpoints", set_num_breakpoints);
-    FLEXT_CADDMETHOD_(thisclass, 0, "h_step", set_h_step);
-    FLEXT_CADDMETHOD_(thisclass, 0, "v_step", set_v_step);
-    FLEXT_CADDMETHOD_(thisclass, 0, "h_pull", set_h_pull);
-    FLEXT_CADDMETHOD_(thisclass, 0, "v_pull", set_v_pull);
-    FLEXT_CADDMETHOD_(thisclass, 0, "linear", set_interpolation_lin);
-    FLEXT_CADDMETHOD_(thisclass, 0, "cubic", set_interpolation_cubic);
-    FLEXT_CADDMETHOD_(thisclass, 0, "spline", set_interpolation_spline);
-    FLEXT_CADDMETHOD_(thisclass, 0, "sinc", set_interpolation_sinc);
-    FLEXT_CADDMETHOD_(thisclass, 0, "debug", set_debug);
-    post("--- gendy~ by Spencer Russell ---");
+	FLEXT_CADDMETHOD(thisclass, 0, set_frequency);
+	FLEXT_CADDMETHOD_(thisclass, 0, "breakpoints", set_num_breakpoints);
+	FLEXT_CADDMETHOD_(thisclass, 0, "h_step", set_h_step);
+	FLEXT_CADDMETHOD_(thisclass, 0, "v_step", set_v_step);
+	FLEXT_CADDMETHOD_(thisclass, 0, "h_pull", set_h_pull);
+	FLEXT_CADDMETHOD_(thisclass, 0, "v_pull", set_v_pull);
+	FLEXT_CADDMETHOD_(thisclass, 0, "linear", set_interpolation_lin);
+	FLEXT_CADDMETHOD_(thisclass, 0, "cubic", set_interpolation_cubic);
+	FLEXT_CADDMETHOD_(thisclass, 0, "spline", set_interpolation_spline);
+	FLEXT_CADDMETHOD_(thisclass, 0, "sinc", set_interpolation_sinc);
+	FLEXT_CADDMETHOD_(thisclass, 0, "debug", set_debug);
+	post("--- gendy~ by Spencer Russell ---");
 }
 
 // Now we define our DSP function. It gets these arguments:
@@ -117,10 +117,10 @@ void gendy::class_setup(t_classid thisclass) {
 
 void gendy::m_signal(int n, float *const *in, float *const *out) {
 	
-    // point outs to the output signal vector
+	// point outs to the output signal vector
 	float *outs = out[0];
-    int filled_index = 0;
-    int retreived_samples = 0;
+	int filled_index = 0;
+	int retreived_samples = 0;
 	
 	waveform.get_wave_data(outs, n);
 	}
